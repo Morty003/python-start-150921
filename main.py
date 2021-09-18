ed8316336f04978bea4c909e91b9c175527782ae
Morty003 <kotfsfs@gmail.com>
2021-09-18 04:52:15 +0300

    dz

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Hello world")
+print("Hello")
+print("World")
+print("!")
+width=int (input("Ширина "))
+lenght=int (input("Высота "))
+print(0.5* width*lenght)
+
+firstnumber=int(input("Первое число "))
+secondnumber=int(input("Второе число "))
+print(firstnumber+secondnumber,firstnumber-secondnumber,firstnumber*secondnumber,firstnumber/secondnumber)
+
+r=int(input("Радиус "))
+import math
+print(2*r,2*math.pi*r,math.pi*r**2 )
